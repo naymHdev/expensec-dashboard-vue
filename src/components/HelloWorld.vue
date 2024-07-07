16ae06d6dddc4ce0f740b0a5f2d29e7ae89a34d1
MacBook Pro <naym100m@gmail.com>
2024-07-07 11:51:45 +0600

    initial setup compleate

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  msg: String,
+});
+</script>
+
+<template>
+  <div class="card">
+    <p class="text-red-500">
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+</template>
